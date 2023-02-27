1cea6687d65b647c65fad9da0004cad0f569c23f
Yu Zhang <zhangy121@student.douglascollege.ca>
2023-02-27 15:09:34 -0800

    update_model

--- public/roff_model/test.py
+++
@@ -1,9 +0,0 @@
-# User inputs the string and it gets stored in the variable str
-str = input("Enter a string: ")
-
-# counter variable to count the character in a string
-counter = 0
-for s in str:
-      counter = counter+1
-
-print("Length of the input string is:", counter)
--- resources/views/forum.blade.php
+++ resources/views/forum.blade.php
@@ -10,7 +10,7 @@
                     </div>
                     <!-- onchange="javascript:location.href = this.value;" -->
                     
-                    <div class='absolute inset-y-0 right-0 px-6'>
+                    <div class='absolute inset-y-0 right-0 px-6 py-4'>
                         
                         <button id="dropdownActionButton" data-dropdown-toggle="dropdownAction" class="inline-flex items-center text-gray-500 bg-white border border-gray-300 focus:outline-none hover:bg-gray-100 focus:ring-4 focus:ring-gray-200 font-medium rounded-md text-sm px-3 py-1.5 dark:bg-gray-800 dark:text-gray-400 dark:border-gray-600 dark:hover:bg-gray-700 dark:hover:border-gray-600 dark:focus:ring-gray-700" type="button">
                             <span class="sr-only">Action button</span>
@@ -18,7 +18,7 @@
                             <!-- <svg class="w-3 h-3 ml-2" aria-hidden="true" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M19 9l-7 7-7-7"></path></svg> -->
                         </button>
                         <!-- Dropdown menu -->
-                        <div id="dropdownAction" class="z-10 hidden bg-white divide-y divide-gray-100 rounded-lg shadow w-44 dark:bg-gray-700 dark:divide-gray-600">
+                        <div id="dropdownAction" class="z-10 hidden bg-white divide-y divide-gray-100 rounded-lg shadow dark:bg-gray-700 dark:divide-gray-600">
                             <ul class="py-1 text-sm text-gray-700 dark:text-gray-200" aria-labelledby="dropdownActionButton">
                                 <li>
                                     <form action="{{ route('forums.indexView') }}" method="POST">
