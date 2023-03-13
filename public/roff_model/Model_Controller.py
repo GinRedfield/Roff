37be65a86c134ececfee110b8fc776847465d072
merry <merryl.tuscano@gmail.com>
2023-03-13 13:24:51 -0700

    stocks_page_update

--- public/roff_model/Model_Controller.py
+++ public/roff_model/Model_Controller.py
@@ -4,7 +4,7 @@
 # Import
 import numpy as np
 import pandas as pd
-from sklearn.model_selection import train_test_split
+# from sklearn.model_selection import train_test_split
 import tensorflow as tf
 from tensorflow import keras
 
